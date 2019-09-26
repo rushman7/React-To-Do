627d19e1057579b5697205667ace03ea8daa24d5
BuildTools <arthurpisakhov1@gmail.com>
2019-09-25 20:54:58 -0400

    setup state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      task: '',
-      id: Date.now(),
-      completed: false
+      todo: [
+        {
+          task: '',
+          id: Date.now(),
+          completed: false
+        }
+      ],
     }
+
+    
   }
 
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
         <h2>Todo List: MVP</h2>
-        <Todo />
+        <Todo data={this.state}/>
       </div>
     );
   }
